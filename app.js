b7d0e0216790f5775cf7ef892224794fa9556183
Saskuatch <asmithamongus@gmail.com>
2020-04-18 22:35:53 -0700

    Added functions to obtain info from user in app.js

--- app.js
+++ app.js
@@ -4,32 +4,117 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
-​
-​
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+const init = function() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What is your employees role?",
+            name: "role",
+            choices: ["Engineer", "Intern", "Manager"]
+        }
+    ]).then(response => {
+        role = response.role
+        switch (role) {
+            case "Engineer":
+                return engiInfo();
+            
+            case "Intern":
+                return internInfo();
+
+            case "Manager":
+                return managerInfo();
+        }
+    })
+}
+
+const engiInfo = function() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the Engineer's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is your Engineer's ID?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is your Engineer's email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is your Engineer's GitHub username?",
+            name: "github"
+        }
+    ]).then(response => {
+        let engi = new Engineer(response.name, response.id, response.email, response.github);
+        return engi;
+    })
+}
+
+const internInfo = function() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the Intern's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is your Intern's ID?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is your Intern's email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is your Intern's school?",
+            name: "school"
+        }
+    ]).then(response => {
+        let intern = new Intern(response.name, response.id, response.email, response.school);
+        return intern;
+    })
+}
+
+const managerInfo = function() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the Manager's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is your Manager's ID?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is your Manager's email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is your Manager's office number?",
+            name: "github"
+        }
+    ]).then(response => {
+        let manager = new Manager(response.name, response.id, response.email, response.github);
+        return manager;
+    })
+}
+init();
